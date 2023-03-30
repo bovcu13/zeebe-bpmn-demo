b503d42d3271d8057d97ce421ce4c7239928769c
bovcu13 <jjps30516@gmail.com>
2023-03-30 23:50:52 +0800

    test

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/zb-user/zb-example/demojob"
 )
 
 func main() {
-	fmt.Println(demojob.FindJobKeysByProcessInstanceKey("2251799813721794"))
+	demojob.FindUserTask()
 }
