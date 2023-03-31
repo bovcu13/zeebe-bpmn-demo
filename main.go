c5aa9f11b662bf79e80de20bfe7ea25d1436df51
bovcu13 <jjps30516@gmail.com>
2023-03-31 18:49:04 +0800

    test

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import (
-	"github.com/zb-user/zb-example/demojob"
-)
+import "github.com/zb-user/zb-example/demojob"
 
 func main() {
-	demojob.FindUserTask()
+	//demojob.GetJobInfoByProcessInstanceKey(2251799813721865)
+	//demojob.FindUserTask()
+	//demojob.StartInstance("choices-subtasks", nil)
+	variables := map[string]interface{}{
+		"meal": "Chicken",
+	}
+	demojob.CompleteJob(2251799813721870, variables)
 }
