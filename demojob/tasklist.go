cfaebdddc7e99248fd367bf44117af359664dd10
bovcu13 <jjps30516@gmail.com>
2023-03-31 18:48:43 +0800

    add info struct add GetJobInfoByProcessInstanceKey update FindUserTask

--- demojob/tasklist.go
+++ demojob/tasklist.go
@@ -2,12 +2,29 @@ package demojob
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/camunda/zeebe/clients/go/v8/pkg/zbc"
 )
+
+type info struct {
+	JobType                  string `json:"jobType"`
+	ProcessId                string `json:"processId"`
+	Key                      int64  `json:"key"`
+	ProcessInstanceKey       int64  `json:"processInstanceKey"`
+	ProcessDefinitionVersion int32  `json:"processDefinitionVersion"`
+	ProcessDefinitionKey     int64  `json:"processDefinitionKey"`
+	Variables                string `json:"variables"`
+	ElementId                string `json:"elementId"`
+	ElementInstanceKey       int64  `json:"elementInstanceKey"`
+	CustomHeaders            string `json:"customHeaders"`
+	Worker                   string `json:"worker"`
+	Retries                  int32  `json:"retries"`
+	Deadline                 int64  `json:"deadline"`
+}
 
 // NewZeebeClient
 // @Description: 建立本地端
@@ -68,8 +85,8 @@ func StartInstance(processId string, variables map[string]interface{}) (string, 
 }
 
 // FindUserTask
-// @Description: 查詢 userTask 待完成任務資訊
-func FindUserTask() {
+// @Description: 查詢 userTask 所有待完成任務詳細資訊
+func FindUserTask() []info {
 	client, err := NewZeebeClient()
 	if err != nil {
 		panic(err)
@@ -77,7 +94,7 @@ func FindUserTask() {
 
 	request := client.NewActivateJobsCommand().
 		JobType("io.camunda.zeebe:userTask").
-		MaxJobsToActivate(10).
+		MaxJobsToActivate(100).
 		Timeout(time.Minute).
 		WorkerName("my-worker")
 
@@ -86,13 +103,74 @@ func FindUserTask() {
 		panic(err)
 	}
 
+	var result []info
 	for _, header := range jobHeaders {
-		log.Printf("ProcessId: %s\n"+
-			"key: %d\n"+
-			"ProcessInstanceKey: %d\n"+
-			"ProcessDefinitionKey: %d\n"+
-			"variables: %v\n", header.GetBpmnProcessId(), header.GetKey(), header.GetProcessInstanceKey(), header.GetProcessDefinitionKey(), header.GetVariables())
+		info := info{
+			JobType:                  header.GetType(),
+			ProcessId:                header.GetBpmnProcessId(),
+			Key:                      header.GetKey(),
+			ProcessInstanceKey:       header.GetProcessInstanceKey(),
+			ProcessDefinitionVersion: header.GetProcessDefinitionVersion(),
+			ProcessDefinitionKey:     header.GetProcessDefinitionKey(),
+			Variables:                header.GetVariables(),
+			ElementId:                header.GetElementId(),
+			ElementInstanceKey:       header.GetElementInstanceKey(),
+			CustomHeaders:            header.GetCustomHeaders(),
+			Worker:                   header.GetWorker(),
+			Retries:                  header.GetRetries(),
+			Deadline:                 header.GetDeadline(),
+		}
+		result = append(result, info)
 	}
+
+	Marshal, _ := json.Marshal(result)
+	fmt.Println(string(Marshal))
+	return result
+}
+
+// GetJobInfoByProcessInstanceKey
+// @Description: 查詢 userTask 中指定的 ProcessInstanceKey 所有待完成任務詳細資訊
+func GetJobInfoByProcessInstanceKey(processInstanceKey int64) []info {
+	client, err := NewZeebeClient()
+	if err != nil {
+		panic(err)
+	}
+
+	request := client.NewActivateJobsCommand().
+		JobType("io.camunda.zeebe:userTask").
+		MaxJobsToActivate(100).
+		Timeout(time.Minute).
+		WorkerName("my-worker")
+
+	jobHeaders, err := request.Send(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	var result []info
+	for _, header := range jobHeaders {
+		if processInstanceKey == header.GetProcessInstanceKey() {
+			info := info{
+				JobType:                  header.GetType(),
+				ProcessId:                header.GetBpmnProcessId(),
+				Key:                      header.GetKey(),
+				ProcessInstanceKey:       header.GetProcessInstanceKey(),
+				ProcessDefinitionVersion: header.GetProcessDefinitionVersion(),
+				ProcessDefinitionKey:     header.GetProcessDefinitionKey(),
+				Variables:                header.GetVariables(),
+				ElementId:                header.GetElementId(),
+				ElementInstanceKey:       header.GetElementInstanceKey(),
+				CustomHeaders:            header.GetCustomHeaders(),
+				Worker:                   header.GetWorker(),
+				Retries:                  header.GetRetries(),
+				Deadline:                 header.GetDeadline(),
+			}
+
+			result = append(result, info)
+		}
+	}
+	fmt.Println(result)
+	return result
 }
 
 // CompleteJob
