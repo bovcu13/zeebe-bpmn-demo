b37b68115e3f0207dc3f3a5d3e06f7a7c47b3633
bovcu13 <jjps30516@gmail.com>
2023-04-10 23:10:11 +0800

    test: 修改以測試部署函數

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
-import "github.com/zb-user/zb-example/demojob"
+import (
+	"fmt"
+	"github.com/zb-user/zb-example/demojob"
+)
 
 func main() {
-	variables := map[string]interface{}{
-		"status": true,
-	}
+	//variables := map[string]interface{}{
+	//	"status": true,
+	//}
 	//demojob.StartInstance("fill-out-timesheets", variables)
 
-	jobInfo := demojob.GetJobInfoByProcessInstanceKey(2251799813722314)
-	if len(jobInfo) == 0 {
-		panic("找不到對應的 jobKey")
-	}
+	//jobInfo := demojob.GetJobInfoByProcessInstanceKey(2251799813722314)
+	//if len(jobInfo) == 0 {
+	//	panic("找不到對應的 jobKey")
+	//}
+	//
+	//jobKey := jobInfo[0].Key // 假設 GetJobInfoByProcessInstanceKey 只返回一个 jobInfo
+	//
+	//demojob.CompleteJob(jobKey, variables)
 
-	jobKey := jobInfo[0].Key // 假設 GetJobInfoByProcessInstanceKey 只返回一个 jobInfo
-
-	demojob.CompleteJob(jobKey, variables)
+	fmt.Println(demojob.DeployBPMNFile("leave.bpmn"))
 
 }
