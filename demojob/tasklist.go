bf63b05f4622abb45a110d2687b3e55b4a99bf8d
bovcu13 <jjps30516@gmail.com>
2023-04-13 18:07:12 +0800

    refactor(任務詳細資訊): 導入[]entities.Job

--- demojob/tasklist.go
+++ demojob/tasklist.go
@@ -2,30 +2,14 @@ package demojob
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"github.com/camunda/zeebe/clients/go/v8/pkg/pb"
 	"log"
 	"time"
 
+	"github.com/camunda/zeebe/clients/go/v8/pkg/entities"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/pb"
 	"github.com/camunda/zeebe/clients/go/v8/pkg/zbc"
 )
-
-type Info struct {
-	JobType                  string `json:"jobType"`
-	ProcessId                string `json:"processId"`
-	Key                      int64  `json:"key"`
-	ProcessInstanceKey       int64  `json:"processInstanceKey"`
-	ProcessDefinitionVersion int32  `json:"processDefinitionVersion"`
-	ProcessDefinitionKey     int64  `json:"processDefinitionKey"`
-	Variables                string `json:"variables"`
-	ElementId                string `json:"elementId"`
-	ElementInstanceKey       int64  `json:"elementInstanceKey"`
-	CustomHeaders            string `json:"customHeaders"`
-	Worker                   string `json:"worker"`
-	Retries                  int32  `json:"retries"`
-	Deadline                 int64  `json:"deadline"`
-}
 
 // NewZeebeClient
 // @Description: 建立本地端
@@ -59,7 +43,7 @@ func DeployBPMNFile(fileName string) (response *pb.DeployResourceResponse) {
 
 // StartInstance
 // @Description: 開啟一個流程實例
-func StartInstance(processId string, variables map[string]interface{}) (string, error) {
+func StartInstance(processId string) (*pb.CreateProcessInstanceResponse, error) {
 	client, err := NewZeebeClient()
 	if err != nil {
 		panic(err)
@@ -67,22 +51,15 @@ func StartInstance(processId string, variables map[string]interface{}) (string, 
 
 	request := client.NewCreateInstanceCommand().BPMNProcessId(processId).LatestVersion()
 
-	if len(variables) > 0 {
-		_, err = request.VariablesFromMap(variables)
-		if err != nil {
-			return "", err
-		}
-	}
-
 	ctx := context.Background()
 	response, err := request.Send(ctx)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	fmt.Println(response.String())
 
-	return response.String(), nil
+	return response, nil
 }
 
 // CancelProcessInstance
@@ -97,16 +74,15 @@ func CancelProcessInstance(processInstanceKey int64) (string, error) {
 	ctx := context.Background()
 	response, err := request.Send(ctx)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to send cancel instance command: %w", err)
 	}
-	fmt.Println(response.String())
-
+	fmt.Printf("Cancelled process instance with key %d. Response: %s\n", processInstanceKey, response.String())
 	return fmt.Sprintf("Successfully cancelled process instance with key %d", processInstanceKey), nil
 }
 
 // FindUserTask
 // @Description: 查詢 userTask 所有待完成任務詳細資訊
-func FindUserTask() []Info {
+func FindUserTask() []entities.Job {
 	client, err := NewZeebeClient()
 	if err != nil {
 		panic(err)
@@ -123,34 +99,12 @@ func FindUserTask() []Info {
 		panic(err)
 	}
 
-	var result []Info
-	for _, header := range jobHeaders {
-		info := Info{
-			JobType:                  header.GetType(),
-			ProcessId:                header.GetBpmnProcessId(),
-			Key:                      header.GetKey(),
-			ProcessInstanceKey:       header.GetProcessInstanceKey(),
-			ProcessDefinitionVersion: header.GetProcessDefinitionVersion(),
-			ProcessDefinitionKey:     header.GetProcessDefinitionKey(),
-			Variables:                header.GetVariables(),
-			ElementId:                header.GetElementId(),
-			ElementInstanceKey:       header.GetElementInstanceKey(),
-			CustomHeaders:            header.GetCustomHeaders(),
-			Worker:                   header.GetWorker(),
-			Retries:                  header.GetRetries(),
-			Deadline:                 header.GetDeadline(),
-		}
-		result = append(result, info)
-	}
-
-	Marshal, _ := json.Marshal(result)
-	fmt.Println(string(Marshal))
-	return result
+	return jobHeaders
 }
 
 // GetJobInfoByProcessInstanceKey
 // @Description: 查詢 userTask 中指定的 ProcessInstanceKey 所有待完成任務詳細資訊
-func GetJobInfoByProcessInstanceKey(processInstanceKey int64) []Info {
+func GetJobInfoByProcessInstanceKey(processInstanceKey int64) []entities.Job {
 	client, err := NewZeebeClient()
 	if err != nil {
 		panic(err)
@@ -167,36 +121,12 @@ func GetJobInfoByProcessInstanceKey(processInstanceKey int64) []Info {
 		panic(err)
 	}
 
-	var result []Info
-	for _, header := range jobHeaders {
-		if processInstanceKey == header.GetProcessInstanceKey() {
-			info := Info{
-				JobType:                  header.GetType(),
-				ProcessId:                header.GetBpmnProcessId(),
-				Key:                      header.GetKey(),
-				ProcessInstanceKey:       header.GetProcessInstanceKey(),
-				ProcessDefinitionVersion: header.GetProcessDefinitionVersion(),
-				ProcessDefinitionKey:     header.GetProcessDefinitionKey(),
-				Variables:                header.GetVariables(),
-				ElementId:                header.GetElementId(),
-				ElementInstanceKey:       header.GetElementInstanceKey(),
-				CustomHeaders:            header.GetCustomHeaders(),
-				Worker:                   header.GetWorker(),
-				Retries:                  header.GetRetries(),
-				Deadline:                 header.GetDeadline(),
-			}
-
-			result = append(result, info)
-		}
-	}
-	Marshal, _ := json.Marshal(result)
-	fmt.Println(string(Marshal))
-	return result
+	return jobHeaders
 }
 
 // GetJobInfoByProcessDefinitionKey
 // @Description: 查詢 userTask 中指定的 ProcessDefinitionKey 所有待完成任務詳細資訊
-func GetJobInfoByProcessDefinitionKey(processDefinitionKey int64) []Info {
+func GetJobInfoByProcessDefinitionKey(processDefinitionKey int64) []entities.Job {
 	client, err := NewZeebeClient()
 	if err != nil {
 		panic(err)
@@ -213,36 +143,12 @@ func GetJobInfoByProcessDefinitionKey(processDefinitionKey int64) []Info {
 		panic(err)
 	}
 
-	var result []Info
-	for _, header := range jobHeaders {
-		if processDefinitionKey == header.GetProcessDefinitionKey() {
-			info := Info{
-				JobType:                  header.GetType(),
-				ProcessId:                header.GetBpmnProcessId(),
-				Key:                      header.GetKey(),
-				ProcessInstanceKey:       header.GetProcessInstanceKey(),
-				ProcessDefinitionVersion: header.GetProcessDefinitionVersion(),
-				ProcessDefinitionKey:     header.GetProcessDefinitionKey(),
-				Variables:                header.GetVariables(),
-				ElementId:                header.GetElementId(),
-				ElementInstanceKey:       header.GetElementInstanceKey(),
-				CustomHeaders:            header.GetCustomHeaders(),
-				Worker:                   header.GetWorker(),
-				Retries:                  header.GetRetries(),
-				Deadline:                 header.GetDeadline(),
-			}
-
-			result = append(result, info)
-		}
-	}
-	Marshal, _ := json.Marshal(result)
-	fmt.Println(string(Marshal))
-	return result
+	return jobHeaders
 }
 
 // GetJobInfoByProcessId
 // @Description: 查詢 userTask 中指定的 processId 所有待完成任務詳細資訊
-func GetJobInfoByProcessId(processId string) []Info {
+func GetJobInfoByProcessId(processId string) []entities.Job {
 	client, err := NewZeebeClient()
 	if err != nil {
 		panic(err)
@@ -259,31 +165,7 @@ func GetJobInfoByProcessId(processId string) []Info {
 		panic(err)
 	}
 
-	var result []Info
-	for _, header := range jobHeaders {
-		if processId == header.GetBpmnProcessId() {
-			info := Info{
-				JobType:                  header.GetType(),
-				ProcessId:                header.GetBpmnProcessId(),
-				Key:                      header.GetKey(),
-				ProcessInstanceKey:       header.GetProcessInstanceKey(),
-				ProcessDefinitionVersion: header.GetProcessDefinitionVersion(),
-				ProcessDefinitionKey:     header.GetProcessDefinitionKey(),
-				Variables:                header.GetVariables(),
-				ElementId:                header.GetElementId(),
-				ElementInstanceKey:       header.GetElementInstanceKey(),
-				CustomHeaders:            header.GetCustomHeaders(),
-				Worker:                   header.GetWorker(),
-				Retries:                  header.GetRetries(),
-				Deadline:                 header.GetDeadline(),
-			}
-
-			result = append(result, info)
-		}
-	}
-	Marshal, _ := json.Marshal(result)
-	fmt.Println(string(Marshal))
-	return result
+	return jobHeaders
 }
 
 // CompleteJob
