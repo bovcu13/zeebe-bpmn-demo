70acc31b22b674a022d422d033eab2867b91832d
bovcu13 <jjps30516@gmail.com>
2023-05-15 19:49:47 +0800

    update

--- tasklist_rest.go
+++ tasklist_rest.go
@@ -1,25 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"net/http"
+	"net/url"
 )
+
+var token = GetAccessToken()
 
 func main() {
 
-	AssignTask("2251799813693381", "demo")
-	variables := []map[string]string{
-		{
-			"name":  "item",
-			"value": "\"Task Assignment\"",
-		},
-	}
-	CompleteTask("2251799813693381", variables)
+	//variables := []map[string]string{
+	//	{
+	//		"name":  "meal",
+	//		"value": "\"Chicken\"",
+	//	},
+	//}
+	//CompleteTask("2251799813685266", variables)
 
 	//GetTaskVariables("2251799813693348")
 
 	//GetAllCreatedTasks()
+
+	//fmt.Println(GetAccessToken())
+
+	AssignTask("2251799813685257", "demo")
+}
+
+// GetAccessToken
+// @Description: 取得Cookie
+func GetAccessToken() string {
+	client := resty.New()
+	body := url.Values{
+		"grant_type":    {"client_credentials"},
+		"client_id":     {"demo"},
+		"client_secret": {"M4SrdxOcznaNm9ZU9OWCb9htxHIdWAzv"},
+	}
+
+	resp, err := client.R().SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetBody(body.Encode()).
+		Post("http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var data map[string]interface{}
+	if err := json.Unmarshal(resp.Body(), &data); err != nil {
+		fmt.Println(err)
+	}
+
+	token, ok := data["access_token"].(string)
+	if !ok {
+		fmt.Println("Unable to get access token")
+	}
+
+	return "Bearer " + token
 }
 
 // GetCookie
@@ -49,10 +86,8 @@ func GetCookie() (name, value string) {
 // @Description: 獲得所有任務詳細資訊
 func GetAllTasks() []byte {
 	client := resty.New()
-	name, value := GetCookie()
 
 	resp, err := client.R().SetHeader("Content-Type", "application/json").
-		SetCookie(&http.Cookie{Name: name, Value: value}).
 		Post("http://localhost:8082/v1/tasks/search")
 
 	if err != nil {
@@ -69,10 +104,8 @@ func GetAllTasks() []byte {
 // @Description: 獲得所有狀態為 Created 任務詳細資訊
 func GetAllCreatedTasks() []byte {
 	client := resty.New()
-	name, value := GetCookie()
 
 	resp, err := client.R().SetHeader("Content-Type", "application/json").
-		SetCookie(&http.Cookie{Name: name, Value: value}).
 		SetBody(map[string]string{
 			"state": "CREATED",
 		}).
@@ -92,10 +125,8 @@ func GetAllCreatedTasks() []byte {
 // @Description: 獲得特定 ID 任務詳細資訊
 func GetSpecificTask(taskId string) []byte {
 	client := resty.New()
-	name, value := GetCookie()
 
 	resp, err := client.R().SetHeader("Content-Type", "application/json").
-		SetCookie(&http.Cookie{Name: name, Value: value}).
 		Get("http://localhost:8082/v1/tasks/" + taskId)
 
 	if err != nil {
@@ -112,10 +143,8 @@ func GetSpecificTask(taskId string) []byte {
 // @Description: 指派任務給使用者
 func AssignTask(taskId string, userName string) []byte {
 	client := resty.New()
-	name, value := GetCookie()
 
-	resp, err := client.R().SetHeader("Content-Type", "application/json").
-		SetCookie(&http.Cookie{Name: name, Value: value}).
+	resp, err := client.R().SetHeader("Content-Type", "application/json").SetHeader("Authorization", token).
 		SetBody(map[string]interface{}{
 			"assignee":                userName,
 			"allowOverrideAssignment": true,
